8ecd9ec7a36e028108ef7650fc87006dbc409893
TransKnight1 <leo.zn_97@hotmail.com>
2024-09-13 14:21:09 -0300

    Gradient as bg?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,31 @@
 import Image from 'next/image';
 import { Header } from './components/header';
+import IconTypescript from './components/icons/typescript-icon';
+import IconNextjs from './components/icons/next-js-icon';
+import IconBxlReact from './components/icons/react-icon';
+import IconBxlTailwindCss from './components/icons/tailwind-icon';
+import IconBxlCss3 from './components/icons/css-icon';
 
 const TechItems = [
-  { name: '', icon: 'path/to/icon' },
-  { name: '', icon: 'path/to/icon' },
-  { name: '', icon: 'path/to/icon' }
+  { name: 'Next.js', icon: <IconNextjs /> },
+  { name: 'Tailwind', icon: <IconBxlTailwindCss /> },
+  { name: 'TypeScript', icon: <IconTypescript /> },
+  { name: 'React', icon: <IconBxlReact /> },
+  { name: 'Css', icon: <IconBxlCss3 /> }
 ];
 
 const Techlogies = () => {
   return (
-    <div>
-      {['React', 'Node', 'TypeScript', 'GraphQL', 'AWS', 'Docker', 'Kubernetes'].map((tech, i) => (
-        <div key={i} className="flex p-2 border rounded-full m-2">
-          <p>{tech}</p>
+    <>
+      {TechItems.map((tech, i) => (
+        <div
+          key={i}
+          className="flex p-2 border-b rounded-full justify-center place-items-center mx-1">
+          <p className="p-1">{tech.icon}</p>
+          <p>{tech.name}</p>
         </div>
       ))}
-    </div>
+    </>
   );
 };
 
@@ -25,12 +35,14 @@ const IntroCard = () => {
       <Image
         src={'/leonardo-zanini.jpg'}
         alt="Leonardo Zanini's picture"
-        className="object-cover w-[40vh] h-[40vh] min-h-64 rounded-l-full"
+        className="object-cover w-[40vh] h-[40vh] min-h-64 rounded-l-full pr-4 shadow-2xl"
         width={600}
         height={1020}
       />
-      <Techlogies />
-      <div className="flex flex-col p-4">
+      <div className="flex flex-col p-4 border-l">
+        <div className="flex justify-start place-items-center mb-4">
+          <Techlogies />
+        </div>
         <h1 className="text-2xl pb-4">Hi i'm Leo!</h1>
         <p>
           I'm a software engineer and I love to code. I'm passionate about technology and I'm always
