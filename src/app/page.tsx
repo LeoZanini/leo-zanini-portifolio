c0a62e9e900e39c5ac6fe67450c75aee334e6749
TransKnight1 <leo.zn_97@hotmail.com>
2024-09-23 16:15:44 -0300

    changes to style, expansive component added, problems with centralization of buttons persists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { Header } from './components/header';
 import IconTypescript from './components/icons/typescript-icon';
@@ -7,80 +8,180 @@ import IconNextjs from './components/icons/next-js-icon';
 import IconBxlReact from './components/icons/react-icon';
 import IconBxlTailwindCss from './components/icons/tailwind-icon';
 import IconBxlCss3 from './components/icons/css-icon';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useMediaQuery } from 'react-responsive';
 
 const TechItems = [
-  { name: 'Next.js', icon: <IconNextjs /> },
-  { name: 'Tailwind', icon: <IconBxlTailwindCss /> },
-  { name: 'TypeScript', icon: <IconTypescript /> },
-  { name: 'React', icon: <IconBxlReact /> },
-  { name: 'CSS', icon: <IconBxlCss3 /> }
+  {
+    name: 'Next.js',
+    icon: <IconNextjs className="w-10 h-10" />,
+    details: 'Experienced in building server-side rendered React applications with Next.js'
+  },
+  {
+    name: 'Tailwind',
+    icon: <IconBxlTailwindCss className="w-10 h-10" />,
+    details: 'Proficient in using Tailwind CSS for rapid UI development'
+  },
+  {
+    name: 'TypeScript',
+    icon: <IconTypescript className="w-10 h-10" />,
+    details: 'Strong TypeScript skills for building type-safe JavaScript applications'
+  },
+  {
+    name: 'React',
+    icon: <IconBxlReact className="w-10 h-10" />,
+    details: 'Expert in React, including hooks, context, and state management'
+  },
+  {
+    name: 'CSS',
+    icon: <IconBxlCss3 className="w-10 h-10" />,
+    details: 'Advanced CSS skills, including flexbox, grid, and animations'
+  }
 ];
 
-const Technologies = () => {
+const Technologies = ({ isExpanded }: { isExpanded: boolean }) => {
+  const [selectedTech, setSelectedTech] = useState<string | null>(null);
+
+  const handleTechClick = (e: React.MouseEvent, techName: string) => {
+    e.stopPropagation();
+    setSelectedTech(selectedTech === techName ? null : techName);
+  };
+
   return (
     <>
-      {TechItems.map((tech, i) => (
-        <motion.div
-          key={i}
-          className="flex hover:bg-white hover:text-purple-950 hover:border-b-purple-500 p-2 border-b-2 shadow-md rounded-full justify-center place-items-center mx-1"
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, delay: i * 0.1 }}
-          whileHover={{ scale: 1.1 }}>
-          <motion.p className="p-1">{tech.icon}</motion.p>
-          <motion.p>{tech.name}</motion.p>
-        </motion.div>
-      ))}
+      {TechItems.map((tech, index) => {
+        const angle = (index * 360) / TechItems.length;
+        const radius = 150; // Slightly larger than half of the circle's diameter
+        const x = Math.cos((angle * Math.PI) / 180) * radius;
+        const y = Math.sin((angle * Math.PI) / 180) * radius;
+
+        return (
+          <motion.button
+            key={tech.name}
+            className="absolute p-2 rounded-full bg-white bg-opacity-10 hover:bg-opacity-20 transition-all duration-300 z-20"
+            style={{
+              left: `calc(50% + ${x}px)`,
+              top: `calc(50% + ${y}px)`,
+              transform: 'translate(10%, 0%)'
+            }}
+            onClick={(e) => handleTechClick(e, tech.name)}
+            whileHover={{ scale: 1.1 }}
+            animate={isExpanded ? { scale: 0, opacity: 0 } : { scale: 1, opacity: 1 }}
+            aria-label={tech.name}>
+            <div className="" title={tech.name}>
+              {tech.icon}
+            </div>
+          </motion.button>
+        );
+      })}
+      <AnimatePresence>
+        {isExpanded && selectedTech && (
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            transition={{ duration: 0.3 }}
+            className="absolute mt-4 p-4 bg-white bg-opacity-10 backdrop-blur-md rounded-lg shadow-lg w-full z-20">
+            <h3 className="text-lg font-semibold mb-2">{selectedTech}</h3>
+            <p>{TechItems.find((tech) => tech.name === selectedTech)?.details}</p>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
 
 const IntroCard = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <motion.div
-      className="flex p-4 border rounded-full w-screen"
-      initial={{ scale: 0.8, opacity: 0 }}
-      animate={{ scale: 1, opacity: 1 }}
-      transition={{ duration: 0.8, ease: 'easeOut' }}>
-      <Image
-        src={'/leonardo-zanini.jpg'}
-        alt="Leonardo Zanini's picture"
-        className="object-cover w-[40vh] h-[40vh] min-h-64 rounded-l-full pr-4 shadow-2xl"
-        width={600}
-        height={1020}
-      />
-
-      <motion.div
-        className="flex flex-col p-4 border-l"
-        initial={{ x: 100, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ duration: 0.8, delay: 0.4 }}>
-        <div className="flex justify-start place-items-center mb-4">
-          <Technologies />
+      className={`relative flex flex-col items-center justify-center transition-all duration-500 ${
+        isExpanded ? 'w-full md:flex-row md:rounded-l-full md:justify-start' : 'w-[340px] h-[340px]'
+      }`}
+      initial={false}
+      animate={isExpanded ? { width: '100%', height: 'auto' } : { width: 340, height: 340 }}>
+      <motion.button
+        className={`relative flex flex-col items-center justify-center bg-black bg-opacity-50 rounded-full overflow-hidden ${
+          isExpanded
+            ? 'w-full md:flex-row md:rounded-l-full md:justify-start'
+            : 'w-[300px] h-[300px]'
+        }`}
+        onClick={() => setIsExpanded(!isExpanded)}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        initial={false}
+        animate={isExpanded ? { width: '100%', height: 'auto' } : { width: 300, height: 300 }}>
+        <div className={`flex justify-center ${isExpanded ? 'md:justify-start mb-4 md:mb-0' : ''}`}>
+          <Image
+            src="/leonardo-zanini.jpg"
+            alt="Leonardo Zanini's picture"
+            className={`object-cover ${
+              isExpanded ? 'w-40 h-40 md:w-64 md:h-64 md:rounded-l-full' : 'w-full h-full'
+            } shadow-2xl border`}
+            width={340}
+            height={340}
+          />
         </div>
-        <motion.h1
-          className="text-2xl pb-4"
-          initial={{ y: 20, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.5, delay: 0.6 }}>
-          Hi, I'm Leo!
-        </motion.h1>
-        <motion.p
-          initial={{ y: 20, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.5, delay: 0.8 }}>
-          I'm a software engineer and I love to code. I'm passionate about technology and I'm always
-          looking for new challenges
-        </motion.p>
-      </motion.div>
+        <motion.div
+          className={`absolute bottom-4 right-4 text-right ${isExpanded ? 'hidden' : ''}`}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8, delay: 0.4 }}>
+          <motion.div
+            className="bg-white bg-opacity-20 backdrop-blur-sm rounded-lg p-2"
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.3 }}>
+            <motion.h1
+              className="text-xl font-bold text-white"
+              initial={{ x: -10, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1 }}>
+              Hi, I'm Leo!
+            </motion.h1>
+            <motion.p
+              initial={{ x: -10, opacity: 0 }}
+              animate={{ x: 0, opacity: 1, scale: isHovered ? 1.1 : 1 }}
+              transition={{ duration: 0.3 }}
+              className="text-sm mt-1 text-white">
+              Click to learn more
+            </motion.p>
+          </motion.div>
+        </motion.div>
+
+        {isExpanded && (
+          <motion.div
+            className="flex flex-col p-4 space-y-4"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.8, delay: 0.4 }}>
+            <motion.h1
+              className="text-2xl text-center md:text-left"
+              initial={{ x: -10, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1 }}>
+              Hi, I'm Leo!
+            </motion.h1>
+            <motion.p
+              initial={{ x: -10, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 2, delay: 1.2 }}
+              className="text-center md:text-left">
+              I'm a software engineer and I love to code. I'm passionate about technology and I'm
+              always looking for new challenges.
+            </motion.p>
+          </motion.div>
+        )}
+      </motion.button>
+      <Technologies isExpanded={isExpanded} />
     </motion.div>
   );
 };
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen w-screen">
+    <div className="flex flex-col min-h-screen">
       <motion.div
         className="flex flex-col justify-center"
         initial={{ y: -50, opacity: 0 }}
@@ -88,25 +189,11 @@ export default function Home() {
         transition={{ duration: 0.5 }}>
         <Header />
       </motion.div>
-      <main className="flex flex-col h-full w-screen place-items-center">
+      <main className="flex flex-col flex-grow items-center justify-center px-4 py-8">
         <IntroCard />
-        <motion.h1
-          className="font-bold text-5xl mt-8"
-          initial={{ scale: 0.5, opacity: 0 }}
-          animate={{ scale: 1, opacity: 1 }}
-          transition={{ duration: 0.5, delay: 1 }}>
-          H E L L O
-        </motion.h1>
-        <motion.h2
-          className="mt-4"
-          initial={{ y: 20, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.5, delay: 1.2 }}>
-          this is a sample text
-        </motion.h2>
       </main>
       <motion.footer
-        className="mt-auto p-4 text-center"
+        className="p-4 text-center"
         initial={{ y: 50, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 1.4 }}>
